c704c08e7151e8eb2ca2cea5370333f1dac9c298
cmbagay <126985167+cmbagay@users.noreply.github.com>
2023-10-29 23:41:37 -0700

    UPLOPAD 2

---
+++ src/Card.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect, useRef } from "react";
+
+function Card() {
+    return (
+        <div>BOI</div>
+    )
+}
+
+export default Card;
--- src/DeckOfCards.jsx
+++ src/DeckOfCards.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
+import Card from "./Card";
 
 function DeckOfCards() {
     return (
-        <div>BOI</div>
+        <div>
+            <button>GIB CARD</button>
+            <Card />
+        </div>
     )
 }
 
-export default DeckOfCards;
+export default DeckOfCards;           
