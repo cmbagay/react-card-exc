a4692581a40375391aede7a0b0d90755355c4bce
cmbagay <126985167+cmbagay@users.noreply.github.com>
2023-11-04 07:13:59 -0700

    added draw card logic + axios install

--- src/Card.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect, useRef } from "react";
-
-function Card() {
-    return (
-        <div>BOI</div>
-    )
-}
-
-export default Card;
--- src/DeckOfCards.jsx
+++ src/DeckOfCards.jsx
@@ -1,11 +1,46 @@
-import React, { useState, useEffect, useRef } from "react";
-import Card from "./Card";
+import React, { useState, useEffect } from "react";
+import axios from "axios"
 
 function DeckOfCards() {
+    const [deck, setDeck] = useState(null);
+    const [drawnCard, setDrawnCard] = useState([])
+
+    useEffect(() => {
+        async function newDeck() {
+            const res = await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle`);
+            setDeck(res.data);
+        }
+        return newDeck()
+    }, [])
+
+    async function drawFromDeck() {
+        const deckRes = await axios.get(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/`)
+
+        if (deckRes.data.remaining > 0) {
+            const card = drawRes.data.cards[0];
+
+            setDrawnCard(drawnDeck => [...drawnDeck,
+            {
+                code: card.code,
+                type: `${card.suit} ${card.value}`,
+                image: card.image
+            }])
+        }
+        else {
+            alert('Error: no cards remaining!');
+        }
+    }
+
+
     return (
-        <div>
-            <button>GIB CARD</button>
-            <Card />
+        <div className="DeckOfCards">
+            <button onClick={drawFromDeck}>GIB CARD</button>
+            <div className="DrawnCards">{
+                drawnCard.map(dk => (
+                    <img className="Card" alt={dk.code} src={image} />
+                ))
+            }
+            </div>
         </div>
     )
 }
